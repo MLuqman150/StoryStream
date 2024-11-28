89bc0e4949890a21922a0c4c7dfa108f5f2c6170
MLuqman150 <muhammadluqman12080@outlook.com>
2024-11-29 00:13:13 +0500

    Connected the database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const mongoose = require('mongoose');
+require('dotenv').config()
 const app = express();
 const port = 5000 || process.env.PORT
 
+// Database connection
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
 
 app.get("/", (req, res) => {
     res.send("Hello World")
