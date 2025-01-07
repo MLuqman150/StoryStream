ccfc6e111612230b55a977675e168af36a69847c
Muhammad Luqman <luqman@cinergiedigital.com>
2025-01-07 17:30:15 +0500

    Added the tailwind css on the frontend

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,9 +1,16 @@
 
 const Login = () => {
     return (
-        <>
-            <h1>Login </h1>
-        </>
+        <div className="flex h-screen items-center justify-center ">
+            <div className="w-[40%] rounded-xl flex flex-col items-center p-4 bg-[#D9EAFD]">
+                <h1 className="font-bold text-2xl">Login </h1>
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" />
+                <label htmlFor="password">Password: </label>
+                <input type="password" name="password" />
+            </div>
+
+        </div>
     )
 }
 
