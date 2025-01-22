a6be8e8d348b392f3827a90413f2ab9bd275c294
Muhammad Luqman <luqman@cinergiedigital.com>
2025-01-22 17:24:23 +0500

    Small change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Login from './components/login/Login'
 import SignUp from './components/signup/SignUp'
 import Home from './components/home/Home'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route index path='/' element={<Login />} />
           <Route path="signup" element={<SignUp />} />
-          <Route path="home" element={<Home />} />
+          <Route path="home" element={<Navbar><Home /></Navbar> } />
         </Routes>
       </BrowserRouter>
     </>
