6343fe8dd81ddd7039d5e9675c388737cad7d9a0
Muhammad Luqman <luqman@cinergiedigital.com>
2025-01-27 15:38:12 +0500

    Added the navbar for small and large screen

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,42 @@
 import { Link, useNavigate } from 'react-router-dom';
+import { IoMenu } from "react-icons/io5";
+import { useState } from 'react';
 
 const Navbar = () => {
     const navigate = useNavigate();
+
+    const [open, setOpen] = useState(false);
 
     const logout = () => {
         navigate("/");
     }
 
+    const dropDown = () =>{
+        setOpen(!open);
+    }
+
     return (
+        
         <div className="m-4 w-[90%] flex justify-between overflow-hidden">
             <h1 className="text-2xl font-bold font-mono">StoryStream</h1>
-            <ul className="flex justify-between w-[30%]">
+            
+            <ul className="md:flex hidden justify-between w-[30%] ">
                 <Link to="/home"><li className="cursor-pointer hover:font-bold">Home</li></Link>
                 <Link to="/home"><li className="cursor-pointer hover:font-bold">Following</li></Link>
                 <Link to="/home"><li className="cursor-pointer hover:font-bold">Settings</li></Link>
             </ul>
-            <button className="bg-blue-500 text-white p-2 rounded-md font-bold hover:bg-blue-600 hover:cursor-pointer" onClick={logout}>Logout</button>
+            <button className="md:block hidden bg-blue-500 text-white p-2 rounded-md font-bold hover:bg-blue-600 hover:cursor-pointer" onClick={logout}>Logout</button>
+            {/* <div> */}
+            <IoMenu className="md:hidden block fixed right-0 mx-2 cursor-pointer text-2xl" onClick={dropDown}  />
+            {open ?<div className={open ? 'sm:block mt-5 right-0 shadow-[5px_5px_5px_rgba(0,0,0,0.25)]  border-2 p-2 rounded-xl' : 'hidden'}>
+                <Link to="/home"><option className="cursor-pointer hover:font-bold list-none">Home</option></Link>
+                <Link to="/home"><li className="cursor-pointer hover:font-bold list-none">Following</li></Link>
+                <Link to="/home"><li className="cursor-pointer hover:font-bold list-none">Settings</li></Link>
+            </div> : <></> }
+            
+            {/* </div> */}
         </div>
+        
     )
 }
 
--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -5,7 +5,10 @@ function Home() {
     return (
         <div>
             <Navbar />
-            <h1>Home</h1>
+            <div className='flex justify-center sm:w-full md:w-[80%] items-center flex-col m-auto bg-[#D9EAFD] rounded-xl h-auto p-2'>
+                <h1 className='text-2xl font-bold font-mono text-blue-700'>Bun vs Deno</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, incidunt dolor hic sunt aut, officiis aliquid vel illum alias, voluptates veritatis nihil tempora pariatur in voluptatibus temporibus dolore consequuntur! Saepe excepturi accusamus tenetur mollitia, neque eum culpa id aut eveniet dolor sed, eius deserunt sit debitis laborum aspernatur itaque ullam.</p>
+            </div>
         </div>
     )
 }
