e454fd48578c4c321194ce2b440d19e4562226a1
Muhammad Luqman <luqman@cinergiedigital.com>
2025-03-26 03:20:32 +0500

    Changes in home page and navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -17,21 +17,25 @@ const Navbar = () => {
 
     return (
         
-        <div className="m-4 w-[90%] flex justify-between overflow-hidden">
-            <h1 className="text-2xl font-bold font-mono">StoryStream</h1>
+        <div className="m-4 w-[90%] flex justify-between overflow-hidden position-relative">
+            <Link to="/home">
+                <h1 className="text-2xl font-bold font-mono cursor-pointer">StoryStream</h1>
+            </Link>
             
-            <ul className="md:flex hidden justify-between w-[30%] ">
+            
+            <ul className="md:flex hidden justify-between w-[30%]">
                 <Link to="/home"><li className="cursor-pointer hover:font-bold">Home</li></Link>
                 <Link to="/home"><li className="cursor-pointer hover:font-bold">Following</li></Link>
                 <Link to="/home"><li className="cursor-pointer hover:font-bold">Settings</li></Link>
             </ul>
             <button className="md:block hidden bg-blue-500 text-white p-2 rounded-md font-bold hover:bg-blue-600 hover:cursor-pointer" onClick={logout}>Logout</button>
             {/* <div> */}
-            <IoMenu className="md:hidden block fixed right-0 mx-2 cursor-pointer text-2xl" onClick={dropDown}  />
-            {open ?<div className={open ? 'sm:block mt-5 right-0 shadow-[5px_5px_5px_rgba(0,0,0,0.25)]  border-2 p-2 rounded-xl' : 'hidden'}>
+            <IoMenu className="md:hidden block fixed right-0 mx-2 cursor-pointer text-2xl"  onClick={dropDown}  />
+            {open ?<div className={open ? 'sm:block mt-5 right-0 shadow-[5px_5px_5px_rgba(0,0,0,0.25)]  border-2 p-2 rounded-xl position-absolute z-100' : 'hidden'}>
                 <Link to="/home"><option className="cursor-pointer hover:font-bold list-none">Home</option></Link>
                 <Link to="/home"><li className="cursor-pointer hover:font-bold list-none">Following</li></Link>
                 <Link to="/home"><li className="cursor-pointer hover:font-bold list-none">Settings</li></Link>
+                <li className="cursor-pointer hover:font-bold list-none" onClick={logout}>Logout</li>
             </div> : <></> }
             
             {/* </div> */}
--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -13,11 +13,11 @@ function Home() {
     return (
         <div>
             <Navbar />
-            <div className='flex justify-center sm:w-full md:w-[70%] items-center flex-row m-auto bg-[#D9EAFD] rounded-xl h-auto p-2'>
-                <img src={bundeno} alt="bundeno" className='w-1/3 m-2 h-auto rounded-xl sm:h-auto' />
+            <div className='flex justify-center sm:w-full md:w-[70%] items-center flex-col m-auto bg-[#D9EAFD] rounded-xl h-auto p-2'>
+                <img src={bundeno} alt="bundeno" className='w-full m-2 h-auto rounded-xl sm:h-auto' />
                 <div>
-                    <h1 className='text-2xl font-bold font-mono text-blue-700'>Bun vs Deno</h1>
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime assumenda et distinctio, at officiis, earum cum delectus corporis ratione odio deserunt, nostrum voluptatibus est ad doloribus consectetur inventore sed sint omnis quas rem libero. Mollitia quibusdam consequuntur veniam blanditiis maxime excepturi ipsam. Commodi quam ad accusantium fuga similique modi atque. Saepe excepturi accusamus tenetur mollitia, neque eum culpa id aut eveniet dolor sed, eius deserunt sit debitis laborum aspernatur itaque ullam.</p>
+                    <h1 className='text-2xl font-bold font-mono text-blue-700 text-center'>Bun vs Deno</h1>
+                    <p className='text-center'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime assumenda et distinctio, at officiis, earum cum delectus corporis ratione odio deserunt, nostrum voluptatibus est ad doloribus consectetur inventore sed sint omnis quas rem libero. Mollitia quibusdam consequuntur veniam blanditiis maxime excepturi ipsam. Commodi quam ad accusantium fuga similique modi atque. Saepe excepturi accusamus tenetur mollitia, neque eum culpa id aut eveniet dolor sed, eius deserunt sit debitis laborum aspernatur itaque ullam.</p>
                 <div className='flex m-2'> 
                     <BiLike className='mx-1 text-blue-700 cursor-pointer' />
                     <BiDislike className='mx-1 text-blue-700 cursor-pointer' />
