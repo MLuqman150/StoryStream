35d2cc771d91afa3ddc61b016437a133367975ed
mluqman150 <muhammadluqman12080@outlook.com>
2026-01-14 01:43:32 +0500

    Implement react quil on create blog page

--- frontend/src/components/createBlog/CreateBlog.jsx
+++ frontend/src/components/createBlog/CreateBlog.jsx
@@ -1,20 +1,46 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Navbar from "../Navbar/Navbar"
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { useAuth } from '../../context/AuthContext';
 import "react-toastify/dist/ReactToastify.css";
 import { toast, ToastContainer } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const CreateBlog = () =>{
 
     const {token, userId} = useAuth()    
 
-    const { register, handleSubmit, reset, formState: { errors, isSubmitting }, setError } = useForm();
+    const { register, handleSubmit, reset, control, formState: { errors, isSubmitting }, setError } = useForm();
 
     const notify = (message) => {
         toast(message);
     }
+
+    // useEffect(() => {
+    //     register("content", { required: true, minLength: 11 });
+    // }, [register]);
+
+    // const onEditorStateChange = (editorState) => {
+    //     setValue("content", editorState);
+    // };
+
+    const [editorHtml, setEditorHtml] = useState('');
+
+    const modules = {
+        toolbar: [
+        [{ 'header': '1' }, { 'header': '2' }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        ['link', 'image'],
+        [{ 'align': [] }],
+        ['clean'],
+        ['code-block'],
+        ],
+    };
+
+    // const editorContent = watch("content");
 
     const navigate = useNavigate();
 
@@ -24,7 +50,8 @@ const CreateBlog = () =>{
         const formData = new FormData()
         formData.append("title", data.title)
         formData.append("image", data.img ? data.img[0] : null )
-        formData.append("content", data.content)
+        // formData.append("content", data.content)
+        formData.append("content", editorHtml)
         formData.append("author", userId)
 
         try{
@@ -75,10 +102,30 @@ const CreateBlog = () =>{
                     <input type="file" accept="image/*" placeholder="Upload your image here" id="img"  {...register("img")} className="border-2 rounded-md w-[75%] text-center h-20"  />
                 </div>
 
-                <div className="flex flex-col justify-center items-center gap-4 m-4 w-[60%]">
+                {/* Implement the fix to fetch the content from the quill editor */}
+                <div className="flex flex-col justify-center items-center gap-4 m-4 w-[60%] h-[300px] mb-20">
                     <label htmlFor="content">Blog Content:</label>
-                    <input type="text" placeholder=" Write you blog content here" id="content" {...register("content",{required:"Content is required"})} className="placeholder-black rounded-md w-[75%] border-2 border-rgb(209,213,220) h-40" />
+                    {/* <input type="text" placeholder=" Write you blog content here" id="content" {...register("content",{required:"Content is required"})} className="placeholder-black rounded-md w-[75%] border-2 border-rgb(209,213,220) h-40" /> */}
+                    <Controller
+                    name="content"
+                    control={control}
+                    render={({ field }) => (
+                      <ReactQuill
+                        theme="snow"
+                        value={field.value}
+                        onChange={(value) => {
+                            setEditorHtml(value)
+                            field.onChange(value)
+                        }}
+                        modules={modules}
+                        className="placeholder-black rounded-md w-[75%] border-rgb(209,213,220) h-[100%]"
+                        // {...register("content", { required: true })}
+                    />
+                    )}
+                  />
+                    
                 </div>
+                {/* <p className="Error">{errors.content && "Enter valid content"}</p> */}
                 {errors.content && <div className="text-red-500 text-center">{errors.content.message}</div>}
                 <div>
                     <button type="submit" disabled={isSubmitting} className="bg-blue-500 text-white font-bold p-2 rounded-xl m-4 w-24 hover:bg-blue-600 hover:cursor-pointer">
